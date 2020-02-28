9316a268f1143148a96a5ce7c805aa2c29215f5c
zura_zhao <1443752623@qq.com>
2020-02-29 00:45:11 +0800

    wrote a function that transpose every element in one tile parallelly

--- code.c
+++ code.c
@@ -9,3 +9,24 @@ const int K=16;
 // to be launched with one thread per element, in (tilesize)x(tilesize) threadblocks
 // thread blocks read & write tiles, in coalesced fashion
 // adjacent threads read adjacent input elements, write adjacent output elmts
+
+__global__ void
+transpose_parallel_per_element_tiled(float in[], float out[])
+{
+    
+    int in_corner_i  = blockIdx.x * K, in_corner_j  = blockIdx.y * K;
+    int out_corner_i = blockIdx.y * K, out_corner_j = blockIdx.x * K;
+
+
+    int x = threadIdx.x, y = threadIdx.y;
+
+
+    __shared__ float tile[K][K];
+
+
+    
+    tile[y][x] = in[(in_corner_i + x) + (in_corner_j + y)*N];
+    __syncthreads();
+    
+    out[(out_corner_i + x) + (out_corner_j + y)*N] = tile[x][y];
+}
