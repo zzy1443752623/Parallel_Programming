a29bd3331595879800dd13a45097509904a91e0a
zura_zhao <1443752623@qq.com>
2020-02-29 00:30:41 +0800

    describing the purpose of this parallel code which is based on the GPU framework named 'CUDA'

--- code.c
+++ code.c
@@ -5,3 +5,7 @@
 
 const int N=1024;  
 const int K=16;
+
+// to be launched with one thread per element, in (tilesize)x(tilesize) threadblocks
+// thread blocks read & write tiles, in coalesced fashion
+// adjacent threads read adjacent input elements, write adjacent output elmts
